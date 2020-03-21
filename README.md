55057e166f92ad7912c645dbabea3aa5258761f2
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2020-03-21 07:04:19 +0100

    initial RDF support

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Archimate Tools
 A collection of scripts that help working with files in the ArchiMate Model Exchange File Format. This is a tool and vendor agnostic file format to exchange Archimate models using XML files. More information on the ArchiMate Model Exchange File Format can be found at: https://www.opengroup.org/open-group-archimate-model-exchange-file-format. When I refer below to a model, I mean a model in the ArchiMate Model Exchange File Format.
 
-More information on Archimate itself can be found at: https://pubs.opengroup.org/architecture/archimate3-doc/
+More information on Archimate itself can be found at: https://pubs.opengroup.org/architecture/archimate3-doc/.
 
 ## archimate-export
 archimate-export is a small command line application that allows to export all the Archimate elements from a model into an Excel file.
 
 Example:
 ```shell
-python archimate.-export.py --output output.xlsx input_model.xml
+python archimate-export.py --output output.xlsx input_model.xml
 ```
---
+++ archimate-export.py
@@ -0,0 +1,72 @@
+import os
+from argparse import ArgumentParser
+
+import xlsxwriter
+from rdflib import Graph, Literal, BNode, RDF, URIRef, Namespace
+from rdflib.namespace import DC, FOAF
+
+import archimate
+
+parser = ArgumentParser(description="archimate-export is a tool that allows to export files in the ArchiMate Model Exchange File Format to a number of other file formats.")
+parser.add_argument('archimateFile', metavar="file", help="The file in  ArchiMate Model Exchange File Format to be used as input.")
+parser.add_argument("--output", "-o", help="The output file to use for the expert.", required=True)
+parser.add_argument("--format", "-f", choices=['rdf', 'xlsx'], default='xlsx', help="The requested output file format.", required=False)
+args = parser.parse_args()
+
+if(not os.path.isfile(args.archimateFile)):
+    print('The file \"%s\" does not exist.' % args.archimateFile)
+    exit(1)
+
+model = archimate.ArchimateModel(args.archimateFile)
+
+elements = model.readElements()
+relationships = model.readRelationships()
+
+if(args.format == "xlxs"):
+    workbook = xlsxwriter.Workbook(args.output)
+    header = workbook.add_format({'bold': True})
+
+    worksheet = workbook.add_worksheet('Export')
+
+    worksheet.write(0, 0, "ID", header)
+    worksheet.write(0, 1, "Name", header)
+    worksheet.write(0, 2, "Element Type", header)
+    worksheet.write(0, 3, "Layer", header)
+    worksheet.write(0, 4, "Aspect", header)
+
+    for index, element in enumerate(elements) :
+        worksheet.write(index + 1, 0, element.identifier)
+        worksheet.write(index + 1, 1, element.name)
+        worksheet.write(index + 1, 2, element.elementType)
+        worksheet.write(index + 1, 3, element.layer)
+        worksheet.write(index + 1, 4, element.aspect)
+
+    workbook.close()
+
+    print('%s elements export to \"%s\"' % (len(elements), args.output))
+elif(args.format == "rdf"):
+    graph = Graph()
+
+    ARCHIMATE = Namespace('http://www.opengroup.org/xsd/archimate/3.0/')
+
+    mapping = {}
+    for element in elements:
+        node = BNode()
+        mapping[element.identifier] = node
+
+        graph.add((node, RDF.type, ARCHIMATE[element.elementType]))
+        graph.add((node, ARCHIMATE.identifier, Literal(element.identifier)))
+        graph.add((node, ARCHIMATE.name, Literal(element.name)))
+
+    for relationship in relationships:
+        node = BNode()
+
+        graph.add((node, RDF.type, ARCHIMATE[relationship.relationshipType]))
+        graph.add((node, ARCHIMATE.source, mapping[relationship.source]))
+        graph.add((node, ARCHIMATE.target, mapping[relationship.target]))
+
+    graph.bind("archimate", ARCHIMATE)
+
+    graph.serialize(args.output)
+
+    print("Archimate model exported in RDF/XML format.")
